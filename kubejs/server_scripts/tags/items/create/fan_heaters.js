56709271141c4f79d8cae5fe69e45fd7bdc0ea3d
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-05-14 20:50:51 +0200

    filter out non-items

--- kubejs/server_scripts/tags/items/create/fan_heaters.js
+++ kubejs/server_scripts/tags/items/create/fan_heaters.js
@@ -1,3 +1,6 @@
 events.listen('item.tags', (event) => {
-    event.add('create:fan_heaters', fan_heaters);
+    event.add(
+        'create:fan_heaters',
+        fan_heaters.filter((item) => item != 'minecraft:lava' && item != 'create:lit_blaze_burner')
+    );
 });

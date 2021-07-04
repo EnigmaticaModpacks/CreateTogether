c49ee3037f25b5083075c76e68b50a5a43deb958
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2021-07-04 23:25:56 +0200

    hide new paxels

--- kubejs/client_scripts/jei_hide_items.js
+++ kubejs/client_scripts/jei_hide_items.js
@@ -1,11 +1,16 @@
 onEvent('jei.hide.items', (event) => {
     const items = [
+        /easypaxellite:(?!wood_paxel|stone_paxel|iron_paxel|golden_paxel|diamond_paxel|netherite_paxel)\w+/,
+
+        /quark:\w+_shard/,
         /quark:limestone/,
         /quark:\w+_limestone/,
+
         /refinedstorage:(?!controller|cable|crafting_grid|external_storage)\w+/,
-        /quark:\w+_shard/,
-        /theoneprobe:/,
-        /simplybackpacks:(?!common)\w+/
+
+        /simplybackpacks:(?!common)\w+/,
+
+        /theoneprobe:/
     ];
 
     items.forEach((item) => {
